196eba1fc02dc1006bded052d967f7f3693af7a7
lishixiong <lishixiong25@163.com>
2020-08-07 01:42:44 +0800

    first commit

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	
+
+	printf("asd12123123");
+	printf("asd12123123");
+	printf("asd12123123");
+	printf("asd12123123");
+	printf("asd12123123");
+	printf("asd12123123");
+	printf("asd12123123");
+	return 0;
+}
